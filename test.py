f9fe10219b1b24db1e794bc346778a287efae02e
lioo <1203958935@qq.com>
2015-11-20 12:07:20 +0800

    20151120-2

--- test.py
+++ test.py
@@ -5,15 +5,15 @@ from items import *
 from util.dbutil import dbutil
 from util.string2num import string2num
 from sqlalchemy import func
-import datetime
+from datetime import datetime
 
 
 def get_data():
     """
     group_gd = [(Deal_time,count_of_deals))]
                  时段(datetime) ,客流量(int)
-    weathers = {Date_time:[wday, weather_num, Temperature_max, Temperature_min]}
-                时间(string,"20141001"):[一周的第几天(1-7), 天气数值(int), 最高温(int),最低温(int)]
+    weathers = [Date_time, wday, weather_num, Temperature_max, Temperature_min]
+                [时间(string,"20141001"),一周的第几天(1-7), 天气数值(int), 最高温(int),最低温(int)]
     :return:[group_gd, weathers],
     """
     s2num = string2num()
@@ -22,20 +22,30 @@ def get_data():
 
     weathers = session.query(weather).all()
     print len(weathers[0].Weather_day)
-    weathers = {datetime.datetime.strftime(tem.Date_time, "%Y%m%d"):
-                [tem.Date_time.isoweekday(),
+    weathers = [[datetime.strftime(tem.Date_time, "%Y%m%d"),
+                tem.Date_time.isoweekday(),
                  s2num.weather2num(tem.Weather_day),
                  tem.Temperature_max,
                  tem.Temperature_min]
-                for tem in weathers}
-
+                for tem in weathers]
+    group_gd =  [[datetime.strftime(tem[0], "%Y%m%d"),
+                  tem[0].hour,
+                  tem[1]]
+                for tem in group_gd]
     return [group_gd, weathers]
 
 import numpy as np
 
 [group_gd, weathers] = get_data()
 # 构造数据集
-# data = []
-#
-# data = [[tem[1],tem[0].hour, tem2 for tem2 in weathers[datetime.datetime.strftime(tem[0], "%Y%m%d")]] for tem in group_gd]
-# xtrain = np.array(data[:,2:])
+
+group_gd_date = [tem[0] for tem in group_gd]
+group_gd_data = [tem[1:] for tem in group_gd]
+weather_date = [tem[0] for tem in weathers]
+weather_data = [tem[1:] for tem in weathers]
+data = []
+for ind,tem in enumerate(group_gd_data):
+    tem.extend(weather_data[weather_date.index(group_gd_date[ind])])
+    data.append(tem)
+
+xtrain = np.array(data)
